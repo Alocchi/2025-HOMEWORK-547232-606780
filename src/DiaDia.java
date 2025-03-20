6e4151e1e316f6e95f6306d94b3ca518666a4ab0
Alocchi <fra.alocchi@gmail.com>
2025-03-20 17:45:58 +0100

    Initial commit

---
+++ src/DiaDia.java
@@ -0,0 +1,124 @@
+
+
+import java.util.Scanner;
+
+/**
+ * Classe principale di diadia, un semplice gioco di ruolo ambientato al dia.
+ * Per giocare crea un'istanza di questa classe e invoca il metodo gioca
+ *
+ * Questa e' la classe principale crea e istanzia tutte le altre
+ *
+ * @author  docente di POO 
+ *         (da un'idea di Michael Kolling and David J. Barnes) 
+ *          
+ * @version base
+ */
+
+public class DiaDia {
+
+	static final private String MESSAGGIO_BENVENUTO = ""+
+			"Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n" +
+			"Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n"+
+			"I locali sono popolati da strani personaggi, " +
+			"alcuni amici, altri... chissa!\n"+
+			"Ci sono attrezzi che potrebbero servirti nell'impresa:\n"+
+			"puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n" +
+			"o regalarli se pensi che possano ingraziarti qualcuno.\n\n"+
+			"Per conoscere le istruzioni usa il comando 'aiuto'.";
+
+	static final private String[] ELENCO_COMANDI = {"vai", "aiuto", "fine"};
+
+	private Partita partita;
+
+	public DiaDia() {
+		this.partita = new Partita();
+	}
+
+	public void gioca() {
+		String istruzione; 
+		Scanner scannerDiLinee;
+
+		System.out.println(MESSAGGIO_BENVENUTO);
+		scannerDiLinee = new Scanner(System.in);		
+		do		
+			istruzione = scannerDiLinee.nextLine();
+		while (!processaIstruzione(istruzione));
+	}   
+
+
+	/**
+	 * Processa una istruzione 
+	 *
+	 * @return true se l'istruzione e' eseguita e il gioco continua, false altrimenti
+	 */
+
+	private boolean processaIstruzione(String istruzione) {
+		Comando comandoDaEseguire = new Comando(istruzione);
+
+		if(comandoDaEseguire.getNome() == null) {
+			System.out.println("Comando non valido");
+		}
+		else if (comandoDaEseguire.getNome().equals("fine")) {
+			this.fine(); 
+		} else if (comandoDaEseguire.getNome().equals("vai"))
+			this.vai(comandoDaEseguire.getParametro());
+		else if (comandoDaEseguire.getNome().equals("aiuto"))
+			this.aiuto();
+		else
+			System.out.println("Comando sconosciuto");
+		if (this.partita.vinta()) {
+			System.out.println("Hai vinto!");
+		}
+		if(this.partita.getCfu() == 0) {
+			System.out.println("Hai perso!");
+		}
+		return this.partita.isFinita();
+	}
+
+
+	// implementazioni dei comandi dell'utente:
+
+	/**
+	 * Stampa informazioni di aiuto.
+	 */
+	private void aiuto() {
+		for(String comando : ELENCO_COMANDI)
+			System.out.print(comando+"\n");
+		System.out.println();
+	}
+
+	/**
+	 * Cerca di andare in una direzione. Se c'e' una stanza ci entra 
+	 * e ne stampa il nome, altrimenti stampa un messaggio di errore
+	 */
+	private void vai(String direzione) {
+		if(direzione==null)
+			System.out.println("Dove vuoi andare ?");
+		else {
+			Stanza prossimaStanza = null;
+			prossimaStanza = this.partita.getStanzaCorrente().getStanzaAdiacente(direzione);
+			if (prossimaStanza == null) {
+				System.out.println("Direzione inesistente");
+			}
+
+			else {
+				this.partita.setStanzaCorrente(prossimaStanza);
+				int cfu = this.partita.getCfu() - 1;
+				this.partita.setCfu(cfu);
+			}
+		}
+		System.out.println(partita.getStanzaCorrente().getDescrizione());
+	}
+	/**
+	 * Comando "Fine".
+	 */
+	private void fine() {
+		this.partita.setFinita();
+		System.out.println("Grazie di aver giocato!");  // si desidera smettere
+	}
+
+	public static void main(String[] argc) {
+		DiaDia gioco = new DiaDia();
+		gioco.gioca();
+	}
+}
