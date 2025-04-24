3b73a661bbecc12d2f80cbc77aa41f297800c5b1
Alocchi <fra.alocchi@gmail.com>
2025-04-24 21:00:04 +0200

    IOSimulator
    inserita la classe IOSimulator e modificato comando vai test di conseguenza

---
+++ src/it/uniroma3/diadia/IOSimulator.java
@@ -0,0 +1,40 @@
+package it.uniroma3.diadia;
+
+public class IOSimulator implements IO{
+	
+	private String[] input;
+	private String[] output;
+	private int numeroElementi;
+	
+	public IOSimulator(String[] pilaDiComandi) {
+		this();
+		this.input = pilaDiComandi;
+	}
+	
+	public IOSimulator() {
+		this.output = new String[10];
+		this.numeroElementi = 0;
+	}
+
+	public void mostraMessaggio(String messaggio) {
+		this.output[numeroElementi] = messaggio;
+		this.numeroElementi++;
+	}
+
+	public String leggiRiga() {
+		String ris = input[0];
+		for(int i = 1; i < input.length; i++) {
+			input[i-1] = input[i];
+		}
+		return ris;
+	}
+	
+	public String[] getOutput() {
+		return this.output;
+	}
+	
+	public String[] getInput() {
+		return this.input;
+	}
+
+}
--- test/it/uniroma3/diadia/comandi/ComandoVaiTest.java
+++ test/it/uniroma3/diadia/comandi/ComandoVaiTest.java
@@ -7,19 +7,20 @@ import org.junit.jupiter.api.Test;
 
 import it.uniroma3.diadia.IO;
 import it.uniroma3.diadia.IOConsole;
+import it.uniroma3.diadia.IOSimulator;
 import it.uniroma3.diadia.Partita;
 
 class ComandoVaiTest {
 
 	private Comando comando;
 	private Partita partita;
-	private IO io;
+	private IOSimulator io;
 
 	@BeforeEach
 	void setUp() throws Exception{
 		this.comando = new ComandoVai();
 		this.partita = new Partita();
-		this.io = new IOConsole();
+		this.io = new IOSimulator();
 	}
 	
 	@Test
@@ -27,20 +28,19 @@ class ComandoVaiTest {
 		this.comando.setParametro("sud");
 		this.comando.esegui(partita, io);
 		assertEquals(this.partita.getStanzaCorrente().getNome(), "Aula N10");
-		//da modificare una volta implementato IOSimulator
 	}
 	
 	@Test
 	void testVaiInDirezioneNonEsistente() {
 		comando.setParametro("destra");
 		this.comando.esegui(partita, io);
-		//da modificare una volta implementato IOSimulator
+		assertEquals(this.io.getOutput()[0], "Direzione inesistente");
 	}
 	
 	@Test
 	void testVaiInDirezioneNulla() {
 		this.comando.esegui(partita, io);
-		//da modificare una volta implementato IOSimulator
+		assertEquals(this.io.getOutput()[0], "Dove vuoi andare ?\n Devi specificare una direzione");
 	}
 
 }
